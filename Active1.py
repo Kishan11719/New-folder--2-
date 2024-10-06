11f6136ef86ab2b9038a0e335e20ce347597c9b0
Kishan11719 <narendragaddamiitmaths@gmail.com>
2024-10-06 18:21:21 +0530

    python

---
+++ Active1.py
@@ -0,0 +1,20 @@
+name = "penguin"
+age=15
+is_student = True
+weight = 38.9
+
+print("Name :",name)
+print("Data Type of name is",type(name))
+print("Age :",age)
+print("Data Type of age is",type(age))
+print("is_student:",is_student)
+print("Weigh :",weight)
+print("Data Type of weight is",type(weight))
+
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of age is",type(age))
+weigh = int(weight)
+print(weight)
+print("Data Type of weight is",type(weight))
